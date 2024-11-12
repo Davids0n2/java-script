59925e198c09949cbc9a021dc880c160ec4415de
Davids0n2 <davidand.tor007@gmail.com>
2024-11-12 18:25:36 -0300

    aula11 - condições em JS part 1

--- aulas/aula011/ex01.js
+++ aulas/aula011/ex01.js
@@ -1,1 +1,9 @@
-console.log(`O console funcinou corretamente!`)
+var vel = 12
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+
+if(vel > 60){ //Condição simples
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+
+}
+
+console.log(`Dirija sempre o cinto de segurança e com cuidado!`)
