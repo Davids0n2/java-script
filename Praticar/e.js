3ad73755d1e3aa191e1ea34b1c36e32d650dbe63
Davids0n2 <davidand.tor007@gmail.com>
2024-11-16 17:08:43 -0300

    Aula 12 - modulo D
    Aula sobre condições (parte2)

---
+++ Praticar/e.js
@@ -0,0 +1,10 @@
+var hora = 1
+if (hora <= 5){
+    console.log('boa madrugada')
+}else if(hora <= 12){
+    console.log('Bom dia')
+}else if(hora <= 18){
+    console.log('boa tarde')
+}else{
+    console.log('boa noite')
+}
