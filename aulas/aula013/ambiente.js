07f8d0e856776464a59c6a159bc2d335c633ea34
Davids0n2 <davidand.tor007@gmail.com>
2024-12-18 10:48:48 -0300

    primeiros passos aprendendo "Do" e "while"

---
+++ aulas/aula013/ambiente.js
@@ -0,0 +1,18 @@
+var cont = 1 
+while(cont <= 500){
+    console.log(`Passo ${cont}`)
+    cont++ //(c = c + 1)
+}
+
+
+
+
+
+/*
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
+
