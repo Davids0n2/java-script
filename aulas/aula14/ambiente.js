19dd21ee91324c9ef45b86a50b45ca37c6a2f8c0
VanitysWear <davidand.tor007@gmail.com>
2025-08-17 14:03:36 -0300

    up

--- aulas/aula14/ambiente.js
+++ aulas/aula14/ambiente.js
@@ -4,8 +4,12 @@ while(c <= 5){
     c++ //(c = c + 1)
 }
 */
+
 console.log('Vai começar...')
-for(var c  = 1; c <= 5; c++){
+
+for(var c  = 1; c <= 10; c++){
     console.log(c)
 }
 console.log('FIM!')
+
+// for (inicialização; condição; incremento) { bloco de código }
