4b7ad9db9de57dcc4e835d95d51dfd598592a81c
Davids0n2 <davidand.tor007@gmail.com>
2026-02-27 08:13:13 -0300

    Variaveis compostas

---
+++ aulas/aula015/ambiente.js
@@ -0,0 +1,5 @@
+let num = [5 , 8 , 2 , 9 , 3] 
+num.push(1) // num.push(1) adiciona o número 1 ao final do vetor
+num.sort() // num.sort()  ordena os números em ordem crescente
+num.length // num.length mostra a quantidade de elementos do vetor
+console.log(num)
--- aulas/aula015/ambiente02.js
+++ aulas/aula015/ambiente02.js
@@ -1,5 +1,5 @@
 let valores = [ 3 ,1 , 4 , 5, 7 , 9 ]
-
-for (let = resu = 0 ; resu < valores.length ; resu++) {
-    console.log
+valores.sort() // valores.sort() ordena os números em ordem crescente
+for (let  resu = 0 ; resu < valores.length ; resu++) {
+    console.log(`A posição ${resu} tem o valor de ${valores[resu]}`)
 }
